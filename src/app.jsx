f36f4cd82269ab8331359c723c0051278cb7c4c4
JYPs <qkrwndma5@naver.com>
2021-04-08 21:08:31 +0900

    템플릿 소스 커밋

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import "./app.css";
+// import Habit from './components/habit';
+
+function App() {
+  return (
+    // <Habit />
+    <h1>hello!!!</h1>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
+  // React.StrictMode = 자바스크립트의 'use strict' 와 동일, 엄격!!!!!!!, 배포시에는 활성화 되지 않아서 console에 오류가 안나옴~
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
