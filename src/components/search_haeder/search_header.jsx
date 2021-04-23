36469ad544489ea2ec892c59bb78e3f75b31fcf8
JYPs <qkrwndma5@naver.com>
2021-04-23 21:04:02 +0900

    add search function!!

--- src/components/search_haeder/search_header.jsx
+++ src/components/search_haeder/search_header.jsx
@@ -1,15 +1,37 @@
 import styles from "./search_header.module.css";
-import React from "react";
+import React, { useRef } from "react";
 
-const SearchHeader = (props) => {
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img className={styles.img} src="/images/logo.png" alt="logo" />
         <h1 className={styles.title}>Yotube</h1>
       </div>
-      <input className={styles.input} type="search" placeholder="Search..." />
-      <button className={styles.button} type="submit">
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
+        {/* 내 습관은 (e) => this.onClick(e) event를 함수로 넘겨줘서 그걸 받아서 e.key를 찾았을텐데 이렇게도 되는구나!!! */}
         <img
           className={styles.buttonImg}
           src="/images/search.png"
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+// New version --> old version에서 '호이' keyword 검색시 key값이 중복이라는 warning이 발생하여 확인하기 위해 New version으로 변경해봄.
+// 결국 같은 기능임.. 단순히 console 확인하기 위함.
+const VideoItem = ({ video: { snippet, id } }) => {
+  console.log("video id :::", id);
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
+
+// Old version
+// const VideoItem = ({ video: { snippet } }) => (
+//   <li className={styles.container}>
+//     <div className={styles.video}>
+//       <img
+//         className={styles.thumbnail}
+//         src={snippet.thumbnails.medium.url}
+//         alt="video thumbnail"
+//       />
+//       <div className={styles.metadata}>
+//         <p className={styles.title}>{snippet.title}</p>
+//         <p className={styles.channel}>{snippet.channelTitle}</p>
+//       </div>
+//     </div>
+//   </li>
+// );
 
 export default VideoItem;
