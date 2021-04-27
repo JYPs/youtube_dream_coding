c4fd41966c93ee831cde841cfc8a66b77369709d
JYPs <qkrwndma5@naver.com>
2021-04-27 23:45:06 +0900

    make video_detail, iframe 사용 및 pre 태그 사용 해봄

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -3,10 +3,19 @@ import styles from "./video_item.module.css";
 
 // New version --> old version에서 '호이' keyword 검색시 key값이 중복이라는 warning이 발생하여 확인하기 위해 New version으로 변경해봄.
 // 결국 같은 기능임.. 단순히 console 확인하기 위함.
-const VideoItem = ({ video: { snippet, id } }) => {
-  console.log("video id :::", id);
+const VideoItem = ({
+  video,
+  video: { snippet, id },
+  onVideoClick,
+  display,
+}) => {
+  // console.log("video id :::", id, display);
+  const displayType = display === "list" ? styles.list : styles.grid;
   return (
-    <li className={styles.container}>
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
       <div className={styles.video}>
         <img
           className={styles.thumbnail}
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import style from "./video_list.module.css";
 
-const VideoList = (props) => {
+const VideoList = ({ videos, onVideoClick, display }) => {
   return (
-    <ul className={style.videos}>
-      {props.videos.map((video) => (
-        <VideoItem key={video.id} video={video} />
-      ))}
-    </ul>
+    <>
+      <ul className={style.videos}>
+        {videos.map((video) => (
+          <VideoItem
+            key={video.id}
+            video={video}
+            onVideoClick={onVideoClick}
+            display={display}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
